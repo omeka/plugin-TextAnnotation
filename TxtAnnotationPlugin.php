e85e3f46f7d74fdf4e8b0c5841b356bdd4c71dc0
Ken Albers <ken.albers@gmail.com>
2016-03-21 15:31:56 -0400

    Allows for just a basic choice of hypothesis or annotator web services.

--- TxtAnnotationPlugin.php
+++ TxtAnnotationPlugin.php
@@ -9,21 +9,25 @@
  */
 class TxtAnnotationPlugin extends Omeka_Plugin_AbstractPlugin
 {
-    protected $_hooks = array('public_head');
+    protected $_hooks = array('public_head', 'config_form', 'config');
     protected $_filters = array('file_markup');
     public function hookPublicHead($args)
-    {
-        queue_js_url('http://assets.annotateit.org/annotator/v1.2.10/annotator-full.min.js');
-        queue_js_string("
-            jQuery(function ($) {
-            $('#content').annotator()
-            .annotator('setupPlugins')
-            .annotator('addPlugin', 'AnnotoriousImagePlugin');
-            });
-            ");
-        queue_js_file('annotorious.okfn-ken');
-        queue_css_url('http://assets.annotateit.org/annotator/v1.2.10/annotator.min.css');
-        queue_css_file('annotorious');
+    {   
+        if (get_option('txt_annotation_service') == 1) {
+            queue_js_url('https://hypothes.is/embed.js');
+        }
+        else {
+            queue_js_url('http://assets.annotateit.org/annotator/v1.2.10/annotator-full.min.js');
+            
+            queue_js_string("
+                jQuery(function ($) {
+                $('#content').annotator()
+                .annotator('setupPlugins')
+                });
+                ");
+            queue_css_url('http://assets.annotateit.org/annotator/v1.2.10/annotator.min.css');
+        }
+        
     }
     public function filterFileMarkup($html, $args)
     {   
@@ -34,5 +38,20 @@ class TxtAnnotationPlugin extends Omeka_Plugin_AbstractPlugin
             $html =  file_markup($file, $options);
         }     
         return $html;
+    }
+    /**
+     * Display the plugin config form.
+     */
+    public function hookConfigForm()
+    {
+        include 'config_form.php';
+    }
+
+    /**
+     * Set the options from the config form input.
+     */
+    public function hookConfig($args)
+    {
+        set_option('txt_annotation_service', (int)$_POST['txt_annotation_service']);
     }
 }
